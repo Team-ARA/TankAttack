e8840e982c5a86db00dd26ae748e569b16ef8ddd
HristoVutov <vutov23@gmail.com>
2015-03-24 13:41:27 +0200

    Add

---
+++ js/main.js
@@ -0,0 +1,54 @@
+/**
+ * Created by urukhai on 3/24/15.
+ */
+var canvas,
+    ctx;
+
+canvas = document.getElementById('canvas');
+ctx = canvas.getContext('2d');
+
+var pic = new Image();
+pic.src = "images/texture1.jpg";
+
+
+var input = new Input();
+attachListeners(input);
+
+var player = new Player(canvas.width - 50, 100);
+
+
+var previousTime = Date.now();
+
+
+
+//create background image of the canvas
+
+function update() {
+    this.tick();
+    this.render(ctx);
+    requestAnimationFrame(update);
+}
+
+function tick() {
+    movePlayer();
+    player.update();
+}
+
+function render(ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+   
+    
+    player.render(ctx);
+
+}
+
+function movePlayer() {
+    player.movement.right = !!input.right;
+    player.movement.left = !!input.left;
+    player.movement.up = !!input.up;
+    player.movement.down = !!input.down;
+}
+
+
+
+update();
