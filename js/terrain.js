43135e4445d3c078ccbc3c542be9ba91fbf86bd3
dpetrova <daniella_petrova@abv.bg>
2015-03-25 16:34:40 +0200

    Fix it

--- js/terrain.js
+++ js/terrain.js
@@ -1,11 +1,16 @@
 /**
  * Created by urukhai on 3/25/15.
  */
-function createTerrain() {
-    var bgImg = new Image();
-    bgImg.src = "images/texture1.jpg";
-    var pattern = ctx.createPattern(pic, 'repeat');
-    ctx.fillStyle = pattern;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-}
+var Terrain = (function () {
+    function createTerrain() {
+        var bgImg = new Image();
+        bgImg.src = "images/texture1.jpg";
+        var pattern = ctx.createPattern(pic, 'repeat');
+        ctx.fillStyle = pattern;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+    Terrain.prototype.render = function (ctx) {
+        this.animation.draw(ctx);
+    };
+    return Terrain;
+});
