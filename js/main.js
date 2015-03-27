a2d7307608b989f8bc9baa1d6ddf5acc93fd13e4
HristoVutov <vutov23@gmail.com>
2015-03-27 22:45:19 +0200

    Add

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ ctx = canvas.getContext('2d');
 var input = new Input();
 attachListeners(input);
 var terrain = new Terrain(0,0);
-
+var playerLooks = "up";
 var rocks = new Array();
 rocks.push(new Obstacle(100, 300));
 rocks.push(new Obstacle(200, 300));
@@ -20,6 +20,7 @@ rocks.push(new Obstacle(400, 400));
 rocks.push(new Obstacle(100, 450));
 
 var enemies = new Array();
+var bullets = new Array();
 enemies.push(new Enemy(Math.random() * canvas.width, canvas.height - 580));
 var timer;
 function startTimer() {
@@ -48,14 +49,19 @@ function update() {
 }
 
 function tick() {
+  //  bulletIntersectWithEnemy();
+    bulletOutOfCanvas();
     playerOutOfCanvas();
     playerIntersectsWithObstacle();
     movePlayer();
     startTimer();
 
     playerLooking();
-
+    
     player.update();
+    bullets.forEach(function (elem) {
+        elem.update();
+    });
     enemies.forEach(function (element) {
         element.update();
     });
@@ -74,21 +80,75 @@ function render(ctx) {
     rocks.forEach(function(element) {
         element.render(ctx);
     });
-
+    bullets.forEach(function (elem) {
+        elem.render(ctx);
+    });
     player.render(ctx);
     enemies.forEach(function(element) {
         element.render(ctx);
     });
     //enemy.render(ctx);
-
+    
 }
 
+function bulletIntersectWithEnemy() {
+    enemies.forEach(function (element) {
+        bullets.forEach(function (elem) {
+            console.log(1);
+            delete element;
+            enemies.splice(element, 1)
+        });
+    });
+//    enemies.forEach(function (element, index) {
+//        bullets.forEach(function (elem, ind) {
+//            if (elem.boundingBox.intersects(element)) {
+//                bullets.splice(ind, 1);
+//                enemies.splice(index, 1)
+//            }
+//        });
+//    });
+       
+}
 function movePlayer() {
-    console.log(player.position);
+    //console.log(player.position);
     player.movement.right = !!input.right;
     player.movement.left = !!input.left;
     player.movement.up = !!input.up;
     player.movement.down = !!input.down;
+    if (input.space) {
+        if (player.watchPos.right == true) {
+            playerLooks = "right";
+        }
+        if (player.watchPos.left == true) {
+            playerLooks = "left";
+        }
+        if (player.watchPos.up == true) {
+            playerLooks = "up";
+        }
+       
+        if (player.watchPos.down == true) {
+            playerLooks = "down";
+        }
+        bullets.push(new Bullet(player.position.x, player.position.y + 10, playerLooks));
+        
+    }
+}
+function bulletOutOfCanvas() {
+    bullets.forEach(function (item, index) {
+        if (item.position.x == 3) {
+            bullets.splice(index, 1);
+        }
+        if (item.position.x > 570) {
+            bullets.splice(index, 1);
+        }
+        if (item.position.y < 2) {
+            bullets.splice(index, 1);
+        }
+        if (item.position.y > 570) {
+            bullets.splice(index, 1);
+        }
+    });
+    
 }
 function playerOutOfCanvas() {
     if (player.position.x < 2) {
